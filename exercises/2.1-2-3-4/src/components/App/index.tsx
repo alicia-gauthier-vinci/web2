366cb534c9f5b668d4670d870f4889d0ae13401b
gauthier <alicia.gauthier@student.vinci.be>
2024-10-24 19:44:48 +0200

    new:ex2.4

--- exercises/2.1-2-3-4/src/components/App/index.tsx
+++ exercises/2.1-2-3-4/src/components/App/index.tsx
@@ -1,5 +1,7 @@
 import PageTitle from "../PageTitle";
 import Cinema from "../Cinema";
+import Header from "../Header"
+import Footer from "../Footer";
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
@@ -48,11 +50,19 @@ const App = () => {
 
   return (
     <div>
+      <Header image="https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+        <h1>"Les meilleurs films"</h1>
+      </Header>
+
       <PageTitle title={pageTitle} />
 
       <Cinema name={cinema1Name} movies={moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+
+      <Footer image="https://media.istockphoto.com/id/1202770152/fr/photo/bobine-de-film-disolement-sur-le-fond-jaune-lumineux-dans-les-couleurs-pastel.jpg?s=1024x1024&w=is&k=20&c=2yKBrC8oyimPdW-5IxFWN_zxFPVK3KWYL9OE2gVmVX4=">
+        <p>© 2021 UGC Cinémas</p>
+      </Footer>
     </div>
   );
 };
---
+++ exercises/2.1-2-3-4/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+interface HeaderProps {
+  image: string;
+  children: React.ReactNode;
+}
+
+const Header = (props: HeaderProps) => {
+  return(
+    <div className="header">
+      <img src={props.image} className="header-image" width="400"/>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+export default Header;
