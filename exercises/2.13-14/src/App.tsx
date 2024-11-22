2482055e05576c045bbe12dc2c7df0d67eeae8fa
gauthier <alicia.gauthier@student.vinci.be>
2024-11-22 10:16:09 +0100

    new:ex2.14

--- exercises/2.13-14/src/App.tsx
+++ exercises/2.13-14/src/App.tsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { useEffect } from 'react';
 import { Joke } from "../types"
 
 function App() {
   const [joke, setJoke] = useState<Joke>();
 
   useEffect(() => {
-    fetch("https://v2.jokeapi.dev/joke/Any?type=single")
-      .then((response) => {
-        if (!response.ok)
-          throw new Error(
-            `fetch error : ${response.status} : ${response.statusText}`
-          );
-        return response.json();
-      })
-      //récupère le body dans setJoke, on récupère la catégorie et la blague dans la vue
-      .then((result) => setJoke(result))
-      .catch((err) => {
-        console.error("HomePage::error: ", err);
-      });
-  }, []);
+    const fetchJoke = () => {
+      fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+        .then((response) => {
+          if (!response.ok)
+            throw new Error(
+              `fetch error : ${response.status} : ${response.statusText}`
+            );
+          return response.json();
+        })
+        .then((result) => {
+          setJoke(result);
+        })
+        .catch((err) => {
+          console.error("HomePage::error: ", err);
+        });
+    };
 
+    fetchJoke();
+    const interval = setInterval(fetchJoke, 5000);
+    return () => clearInterval(interval);
+  }, []);
+      
   return (
     <>
       <p> <strong>Joke category :</strong> {joke?.category}</p>
